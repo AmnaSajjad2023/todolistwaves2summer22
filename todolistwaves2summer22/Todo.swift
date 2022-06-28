9acc6c9ed7c1d3a9d6f429b12a2c508065d1ed1c
amnaSajjad <amnasajjad2023@gmail.com>
2022-06-28 20:38:05 +0500

    Initial Commit

---
+++ todolistwaves2summer22/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  todolistwaves2summer22
+//
+//  Created by Amna Sajjad on 2022-06-28.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ todolistwaves2summer22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todolistwaves2summer22
+//
+//  Created by Amna Sajjad on 2022-06-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
